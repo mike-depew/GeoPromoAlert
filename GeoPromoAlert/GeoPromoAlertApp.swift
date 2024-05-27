f5b7e749cf240b2ebebf55186d6c3b45370591cc
Mike Depew <mike.depew.developer@gmail.com>
2024-05-27 15:17:41 -0700

    Initial Commit

---
+++ GeoPromoAlert/GeoPromoAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoPromoAlertApp.swift
+//  GeoPromoAlert
+//
+//  Created by Admin on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct GeoPromoAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
